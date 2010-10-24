1999ee877b62df2ceefce257a61f931c464ec84c
bluerabbit.com@gmail.com <bluerabbit.com@gmail.com@7859c1e0-835d-3019-3c91-f52c375c6c12>
2010-10-24 13:24:08 +0000

    リファクタリング
    git-svn-id: http://atndnotify.googlecode.com/svn/trunk@15 7859c1e0-835d-3019-3c91-f52c375c6c12

--- js/atnd.js
+++ js/atnd.js
@@ -53,58 +53,70 @@ Atnd.prototype = {
 			}
 		});
 		return result;
-	},
-	find : function(param) {
-		param['format'] = "json";
-		// 直近三ヶ月を検索対象とする
-		var date = new Date();
-		param['ym'] = toYyyyMm(date) + ',' + toYyyyMm(nextMonth(date)) + ',' + toYyyyMm(nextMonth(date));
-
-		function toYyyyMm(date) {
-			var mm = date.getMonth() + 1;
-			if (mm < 10) mm = "0" + mm;
-			return String(date.getFullYear()) + mm;
-		}
-
-		function nextMonth(date) {
-			date.setMonth(date.getMonth()+1);
-			return date;
-		}
-
-		function toDate(str) {
-			var yyyymmdd = str.substring(0, 10); // 2009-01-31
-			var HhMiSS = str.substring(11, 18); // 20:44:34
-			var time = Date.parse(yyyymmdd + " " + HhMiSS); // "2009/01/31 20:44:34"
-			var date = new Date();
-			date.setTime(time);
-			return date;
-		}
-
-		var result = [];
-		$.ajax( {
-			type : "GET",
-			url : "http://api.atnd.org/events/",
-			data : param,
-			dataType : "json",
-			async : false,
-			success : function(json) {
-				var now = new Date();
-				// 終わってるイベントは除く
-				json.events.forEach(function (event, i) {
-					var endDate = toDate(event.ended_at);
-					if (now.getTime() < endDate.getTime()) {
-						result.push(event);
-					}
-				});
-			},
-			error: function (xhr, errorText, error) {
-				if (xhr.status == 200) {
-					console.warn("Atnd.find error:" + errorText);
-					console.warn(xhr);
-					console.warn(error);
-				}
-			}
-		});
-		return result;
 	}
 };
+
+Atnd.findByKeyword = function(keyword) {
+	var date = new Date();
+	// 直近三ヶ月を検索対象とする
+	var param = {
+			keyword: keyword,
+			format: 'json',
+			ym: toYyyyMm(date) + ',' + toYyyyMm(nextMonth(date)) + ',' + toYyyyMm(nextMonth(date))
+	};
+	function toYyyyMm(date) {
+		var mm = date.getMonth() + 1;
+		if (mm < 10) mm = "0" + mm;
+		return String(date.getFullYear()) + mm;
+	}
+
+	function nextMonth(date) {
+		date.setMonth(date.getMonth()+1);
+		return date;
+	}
+
+	var result = [];
+	$.ajax( {
+		type : "GET",
+		url : "http://api.atnd.org/events/",
+		data : param,
+		dataType : "json",
+		async : false,
+		success : function(json) {
+			var now = new Date();
+			// 終了したイベントは検索結果に含まない
+			json.events.forEach(function (event, i) {
+				if (!event.ended_at) {
+					// 終了日が設定されていないイベントは開始日から三日経過してたら終わった事にする
+					var endTime = Atnd.toDate(event.started_at).getTime() + (1000 * 60 * 60 * 24 * 3);
+					if (now.getTime() < endTime) {
+						result.push(event);
+					}
+					return;
+				}
+				var endDate = Atnd.toDate(event.ended_at);
+				if (now.getTime() < endDate.getTime()) {
+					result.push(event);
+				}
+			});
+		},
+		error: function (xhr, errorText, error) {
+			if (xhr.status == 200) {
+				console.warn("Atnd.find error:" + errorText);
+				console.warn(xhr);
+				console.warn(error);
+			}
+		}
+	});
+	return result;
+}
+
+//ATND APIレスポンスの日付文字列（started_at, ended_at ..etc)[yyyy-mm-ddThh:mi:ss+09:00]をDate型に変換する
+Atnd.toDate = function toDate(str) {
+	var yyyymmdd = str.substring(0, 10);
+	var HhMiSS = str.substring(11, 18);
+	var time = Date.parse(yyyymmdd + " " + HhMiSS);
+	var date = new Date();
+	date.setTime(time);
+	return date;
+}
